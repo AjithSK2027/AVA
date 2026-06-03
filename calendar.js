dcde960b2e66a7e1dfb956456d79d48cf7d79d9f
AjithSK2027 <adominic906@gmail.com>
2026-06-03 13:11:10 +0530

    Add buildCalendar function to create calendar header

---
+++ calendar.js
@@ -0,0 +1,11 @@
+function buildCalendar(){
+
+    const board =
+        document.getElementById(
+            "calendarBoard"
+        );
+
+    board.innerHTML =
+        "<h2>Calendar Coming Next</h2>";
+
+}
